db70931e366b41684444548b38928032e0774989
joaoppagnan <joaopedroopagnan@gmail.com>
2022-06-16 18:46:32 -0300

    Adding .csv data

--- src/data/process_data.py
+++ src/data/process_data.py
@@ -1,3 +1,16 @@
 import numpy as np
 import pandas as pd
+import pywt
 
+def denoise(data): 
+    w = pywt.Wavelet('sym4')
+    maxlev = pywt.dwt_max_level(len(data), w.dec_len)
+    threshold = 0.04 # Threshold for filtering
+
+    coeffs = pywt.wavedec(data, 'sym4', level=maxlev)
+    for i in range(1, len(coeffs)):
+        coeffs[i] = pywt.threshold(coeffs[i], threshold*max(coeffs[i]))
+        
+    datarec = pywt.waverec(coeffs, 'sym4')
+    
+    return datarec
