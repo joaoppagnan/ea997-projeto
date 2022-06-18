b763ad0c564d2a0dd27d326f02a6c9694df1a53f
joaoppagnan <joaopedroopagnan@gmail.com>
2022-06-18 13:43:36 -0300

    Did function to apply FFT

--- src/features/fft.py
+++ src/features/fft.py
@@ -1,1 +1,11 @@
 import numpy as np
+
+class FFT_Transform():
+    def __init__(self, X_data:np.ndarray):
+        self.X_data = X_data
+
+    def transform(self):
+            self.X_fft = [np.abs(np.fft.fft(x)) for x in self.X_data]
+
+    def get_transformed_data(self):
+        return self.X_fft
