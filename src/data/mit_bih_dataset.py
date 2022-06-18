7481521ac1a2dd1cfc7b5e69beca5dd38eaa7ab6
joaoppagnan <joaopedroopagnan@gmail.com>
2022-06-18 14:53:58 -0300

    Updated dataset class

--- src/data/mit_bih_dataset.py
+++ src/data/mit_bih_dataset.py
@@ -3,7 +3,7 @@ import pandas as pd
 import os
 
 class MIT_BIH_Dataset:
-    def __init__(self, path:str):
+    def __init__(self, path:str, window_size:int=180):
         self.path = path
         self.classes = ['N', 'L', 'R', 'A', 'V']
         self.n_classes = len(self.classes)
@@ -35,7 +35,43 @@ class MIT_BIH_Dataset:
         pass
 
     def extract_data(self):
+        for r in range(0,len(self.records)):
+            signals = []
+
+            with open(self.records[r], 'rt') as csvfile:
+                spamreader = pd.read_csv(csvfile, delimiter=',', quotechar='|') # read CSV file\
+                row_index = -1
+                for row in spamreader:
+                    if(row_index >= 0):
+                        signals.insert(row_index, int(row[1]))
+                    row_index += 1
+
+            # Read anotations: R position and Arrhythmia class
+            example_beat_printed = False
+            with open(self.annotations[r], 'r') as fileID:
+                data = fileID.readlines() 
+                beat = list()
+
+                for d in range(1, len(data)): # 0 index is Chart Head
+                    splitted = data[d].split(' ')
+                    splitted = filter(None, splitted)
+                    next(splitted) # Time... Clipping
+                    pos = int(next(splitted)) # Sample ID
+                    arrhythmia_type = next(splitted) # Type
+                    if(arrhythmia_type in self.classes):
+                        arrhythmia_index = self.classes.index(arrhythmia_type)
+                        self.count_classes[arrhythmia_index] += 1
+                        if(self.window_size <= pos and pos < (len(signals) - self.window_size)):
+                            beat = signals[pos-self.window_size:pos+self.window_size]     ## REPLACE WITH R-PEAK DETECTION
+                            self.X_data.append(beat)
+                            self.y_data.append(arrhythmia_index)
         pass
 
     def get_filenames_annotations(self):
         return (self.filenames, self.annotations)
+
+    def get_X_data(self):
+        return self.X_data
+
+    def get_y_data(self):
+        return self.y_data
