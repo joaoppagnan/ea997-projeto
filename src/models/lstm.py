c072f8c57bd9ee7e8104f93995d9c1bbb808ca32
joaoppagnan <joaopedroopagnan@gmail.com>
2022-06-18 14:28:46 -0300

    Updated LSTM model

--- src/models/lstm.py
+++ src/models/lstm.py
@@ -4,4 +4,21 @@ import tensorflow as tf
 from tensorflow import keras
 
 class LSTM_Classifier:
-    pass
+    def __init__(self, X_train:np.ndarray, y_train:np.ndarray, input_shape:tuple, n_neurons:int = 64):
+        self.X_train = X_train
+        self.y_train = y_train
+        self.input_shape = input_shape
+        self.n_neurons = n_neurons
+        pass
+
+    def create_model(self):
+        model = keras.Sequential()
+        model.add(keras.Input(shape=self.input_shape))
+        model.add(keras.layers.LSTM(self.n_neurons, activation='tanh',
+                                    kernel_initializer=self.init_mode, return_sequences=self.return_sequences,
+                                    name="camada_lstm"))
+
+        pass
+
+    def build(self):
+        pass
