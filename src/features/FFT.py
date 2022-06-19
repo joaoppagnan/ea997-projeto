317dcc3909467e2c493514a30354522e47d5da39
joaoppagnan <joaopedroopagnan@gmail.com>
2022-06-18 23:30:42 -0300

    Did the evaluation for the frequency domain

--- src/features/FFT.py
+++ src/features/FFT.py
@@ -5,7 +5,7 @@ class FFT():
         self.X_data = X_data
 
     def transform(self):
-            self.X_fft = [np.abs(np.fft.fft(x)) for x in self.X_data]
+            self.X_fft = [np.abs(np.fft.fft(x))[:int(len(x)/2)] for x in self.X_data]
 
     def get_transformed_data(self):
         return self.X_fft
